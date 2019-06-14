6a29525f0f2da0c0983aef5a4710dc7c0e792217
imaginist <imaginist@sjtu.edu.cn>
2019-06-14 14:17:39 +0800

    change a little create_data.py

--- create_data.py
+++ create_data.py
@@ -1,11 +1,13 @@
 import os
 
 father_path = "/home/data/imaginist_data/Trajectory/cropped_left/"
-patient_list = os.listdir(father_path)
+patient_list = sorted(os.listdir(father_path))
 for patient in patient_list:
+    print(patient)
     folder_index = 0
     count = 0
     image_list = os.listdir(father_path + patient)
+    image_list = sorted(image_list)
     for image in image_list:
         image_path = father_path + patient + '/' + image
-        print(image_path)
+        # print(image_path)
