aff74711c0a2393412eadf47fa2174d4f26bf70b
imaginist <imaginist@sjtu.edu.cn>
2019-06-14 22:38:55 +0800

    add test and train dataset to Mydataset

--- MyDataset.py
+++ MyDataset.py
@@ -9,17 +9,36 @@ FramePath = "/home/data/imaginist_data/Trajectory/training_data/"
 LabelPath = "./label.csv"
 
 class ParkinsonDataset(Dataset):
-    def __init__(self, data_type='filter'):
+    def __init__(self, data_type='train'):
         self.data_path = []
         self.label = []
-        with open(LabelPath) as csvfile:
-            csv_reader = csv.reader(csvfile)  # 使用csv.reader读取csvfile中的文件
-            for row in csv_reader:  # 将csv文件中的数据保存到birth_data中
-                # print(row)
-                patient_path = FramePath + row[0] + "_" + row[1] + "/"
-                for data_index in sorted(os.listdir(patient_path)):
-                    self.data_path.append(patient_path+data_index)
-                    self.label.append(int(row[2]))
+        if data_type == 'train':
+            with open(LabelPath) as csvfile:
+                csv_reader = csv.reader(csvfile)  # 使用csv.reader读取csvfile中的文件
+                count = 0
+                for row in csv_reader:  # 将csv文件中的数据保存到birth_data中
+                    # print(row)
+                    if count >= 80:
+                        break
+                    patient_path = FramePath + row[0] + "_" + row[1] + "/"
+                    for data_index in sorted(os.listdir(patient_path)):
+                        self.data_path.append(patient_path+data_index)
+                        self.label.append(int(row[2]))
+                    count += 1
+
+        if data_type == 'test':
+            with open(LabelPath) as csvfile:
+                csv_reader = csv.reader(csvfile)  # 使用csv.reader读取csvfile中的文件
+                count = 0
+                for row in csv_reader:  # 将csv文件中的数据保存到birth_data中
+                    # print(row)
+                    if count < 80:
+                        continue
+                    patient_path = FramePath + row[0] + "_" + row[1] + "/"
+                    for data_index in sorted(os.listdir(patient_path)):
+                        self.data_path.append(patient_path+data_index)
+                        self.label.append(int(row[2]))
+                    count += 1
 
 
     def __getitem__(self, index):
@@ -37,8 +56,11 @@ class ParkinsonDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dset_train = ParkinsonDataset()
+    dset_train = ParkinsonDataset(data_type='train')
     train_loader = DataLoader(dset_train, batch_size=1, shuffle=False, num_workers=0)
     print("Training Data : ", len(train_loader.dataset))
-    for batch_idx, (data, label) in enumerate(train_loader):
-        print(data.size, label)
+    # for batch_idx, (data, label) in enumerate(train_loader):
+    #     print(data.size(), label)
+    test_data = ParkinsonDataset(data_type='test')
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=False, num_workers=0)
+    print("Training Data : ", len(test_loader.dataset))
